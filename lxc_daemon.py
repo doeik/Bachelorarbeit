c416f565b42ffc07b793152890b12f23209c2051
doeik <dorian.eikenberg@uni-duesseldorf.de>
2014-06-20 18:25:49 +0200

    lxc_daemon build2

--- lxc_daemon.py
+++ lxc_daemon.py
@@ -1,56 +1,23 @@
 from __future__ import unicode_literals
-import _thread
-import atexit
-import fcntl
 import io
+import optparse
 import os
 import random
 import shutil
 import socket
 import string
 import subprocess
-import sys
 import threading
 
 LXC_PATH = "/var/lib/lxc/"
 TIMEOUT = 30
+CONFIG_TEMPLATE = "./config"
 SERVER_ADDRESS = "./uds_lxcdaemon"
-LOCKFILE = "./.lock_lxcdaemon"
-FD_LOCK = None
-RUNNING = True
-
-
-def cleanup():
-    FD_LOCK.close()
-    os.unlink(LOCKFILE)
-    os.unlink(SERVER_ADDRESS)
-
-
-def handleInstance():
-    global FD_LOCK
-    FD_LOCK = io.open(LOCKFILE, "w")
-    try:
-        fcntl.flock(FD_LOCK, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except IOError:
-        return False
-    return True
-
-
-def composeReply(toClient_socket, returncode, timeout_occured):
-    if timeout_occured:
-        msg = "t/o"
-    else:
-        msg = str(returncode)
-    try:
-        toClient_socket.sendall(msg.encode())
-    except Exception as e:
-        print(e)
-    toClient_socket.close()
 
 
 def makeConfigFromTemplate(tmpName):
     container_path = os.path.join(LXC_PATH, tmpName) + "/"
-    config = io.open("./config", "r")
+    config = io.open(CONFIG_TEMPLATE, "r")
     content = config.readlines()
     config.close()
     for i, line in enumerate(content):
@@ -63,7 +30,12 @@ def makeConfigFromTemplate(tmpName):
     newconf.close()
 
 
-def handleLxc(toClient_socket, params):
+def timerEvent(timeout_event, tmpName):
+        subprocess.call(["lxc-stop", "-n", tmpName, "-k"])
+        timeout_event.set()
+
+
+def processLxc(params):
     program_abspath = params[0]
     program = os.path.basename(program_abspath)
     # Generate random string for naming the temporarely created container
@@ -71,15 +43,10 @@ def handleLxc(toClient_socket, params):
     tmpName = ''.join([random.choice(string.ascii_letters + string.digits)
                        for n in range(12)])
     subprocess.call(["lxc-create", "-n", tmpName, "-t", "debian"])
-    timeout_occured = False
-
-    def timerEvent():
-        subprocess.call(["lxc-stop", "-n", tmpName, "-k"])
-        nonlocal timeout_occured
-        timeout_occured = True
+    timeout_event = threading.Event()
     try:
         makeConfigFromTemplate(tmpName)
-        t = threading.Timer(TIMEOUT, timerEvent)
+        t = threading.Timer(TIMEOUT, timerEvent, (timeout_event, tmpName))
         t.start()
         rootpath = os.path.join(LXC_PATH, tmpName, "rootfs/")
         shutil.copy(program_abspath, rootpath)
@@ -87,30 +54,31 @@ def handleLxc(toClient_socket, params):
                       os.path.join("/", program), "-q"]
         callparams.extend(params[1:])
         returncode = subprocess.call(callparams)
-        #print("Returncode: " + str(returncode))
     except Exception as e:
+        returncode = 2
         print(e)
     finally:
         try:
             t.cancel()
         except:
             pass
-        composeReply(toClient_socket, returncode, timeout_occured)
         subprocess.call(["lxc-destroy", "-n", tmpName])
+    return (returncode, timeout_event.is_set())
 
 
-def evaluateMessage(toClient_socket, text):
-    global RUNNING
-    if text[0] == "!":
-        RUNNING = False
-        msg = "Stopped"
-        try:
-            toClient_socket.sendall(msg.encode())
-        except:
-            pass
-        return
-    params = text[1:].split()
-    _thread.start_new_thread(handleLxc, (toClient_socket, params))
+def composeReplyToClient(client_socket, text):
+    params = text.split()
+    (returncode, timeout_occured) = processLxc(params)
+    msg = str(returncode)
+    if timeout_occured:
+        msg = "t/o"
+    if returncode == 2:
+        msg = "2 - an unexpected exception occured while trying to run the container"
+    try:
+        client_socket.sendall(msg.encode())
+    except Exception as e:
+        print(e)
+    client_socket.close()
 
 
 def runServer():
@@ -121,16 +89,18 @@ def runServer():
     server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     server_socket.bind(SERVER_ADDRESS)
     server_socket.listen(1)
-    while RUNNING:
-        toClient_socket, toClient_address = server_socket.accept()
+    while True:
+        client_socket, toClient_address = server_socket.accept()
         try:
-            tmpstr = toClient_socket.recv(2048)
-            evaluateMessage(toClient_socket, tmpstr.decode())
+            tmpstr = client_socket.recv(2048)
+            client_thread = threading.Thread(
+                target=composeReplyToClient, args=(client_socket, tmpstr.decode()))
+            client_thread.start()
         except:
-            toClient_socket.close()
+            client_socket.close()
 
 
-def startDaemon():
+def sendToBackground():
     pid = os.fork()
     if pid == 0:
         os.setsid()
@@ -146,13 +116,16 @@ def startDaemon():
 
 
 def main():
-    if handleInstance() == True:
-        atexit.register(cleanup)
-        if len(sys.argv) > 1:
-            if sys.argv[1] == "debug":
-                runServer()
-                return
-        startDaemon()
+    parser = optparse.OptionParser()
+    parser.add_option("-b", "--background", action="store_true",
+                      dest="background", help="start this process in the background")
+    parser.add_option("-f", "--foreground", action="store_true",
+                      dest="foreground", default=True, help="start this process in the foreground")
+    (options, args) = parser.parse_args()
+    if options.background:
+        sendToBackground()
+    if options.foreground:
+        runServer()
 
 if __name__ == "__main__":
     main()
--- lxc_daemon_client.py
+++ lxc_daemon_client.py
@@ -6,21 +6,19 @@ SERVER_ADDRESS = "./uds_lxcdaemon"
 
 
 def main():
-    msg = "#" + os.path.abspath(sys.argv[1])
-    if sys.argv[1] == "stop":
-        msg = "!"
-    toServer_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    msg = os.path.abspath(sys.argv[1])
+    client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     try:
-    	toServer_socket.connect(SERVER_ADDRESS)
+    	client_socket.connect(SERVER_ADDRESS)
     except Exception as e:
     	print(e)
     else:
     	try:
-    		toServer_socket.sendall(msg.encode())
+    		client_socket.sendall(msg.encode())
     	except Exception as e:
     		print(e)
     	else:
-            reply = toServer_socket.recv(16).decode()
+            reply = client_socket.recv(16).decode()
             print("lxc_daemon_client: " + reply)
 
 
