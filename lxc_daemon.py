ba0416ad31b4a6977ae9621f2be368b88170fca5
doeik <dorian.eikenberg@uni-duesseldorf.de>
2014-06-26 18:17:08 +0200

    ich könnte heulen

--- lxc_daemon.py
+++ lxc_daemon.py
@@ -1,19 +1,21 @@
 from __future__ import unicode_literals
+import base64
 import io
 import json
+import lxc
 import optparse
 import os
 import random
-import shutil
 import socket
+import stat
 import string
 import subprocess
 import threading
 
 LXC_PATH = "/var/lib/lxc/"
-TIMEOUT = 30
+TIMEOUT = 10
 CONFIG_TEMPLATE = "./config"
-SERVER_ADDRESS = "./uds_lxcdaemon"
+UDS_SOCKET = "./uds_lxcdaemon"
 
 
 def makeConfigFromTemplate(tmpName):
@@ -31,30 +33,46 @@ def makeConfigFromTemplate(tmpName):
     newconf.close()
 
 
-def timerEvent(timeout_event, tmpName):
-        subprocess.call(["lxc-stop", "-n", tmpName, "-k"])
-        timeout_event.set()
+def timerEvent2(timeout_event, tmpName):
+    timeout_event.set()
+    bla = io.open("timertest", "w")
+    bla.write("test")
+    bla.close
+    subprocess.call(["lxc-stop", "-n", tmpName, "-k"])
 
 
-def processLxc(params):
-    program_abspath = params[0]
-    program = os.path.basename(program_abspath)
-    # Generate random string for naming the temporarely created container
-    # TO DO: tempfile
-    tmpName = ''.join([random.choice(string.ascii_letters + string.digits)
+def timerEvent(timeout_event, container):
+    timeout_event.set()
+    print("Container state: " + container.state)
+    container.stop()
+    print("Container state: " + container.state)
+
+
+def processLxc(dict_request, program):
+    params = dict_request["params"]
+    if "timeout" in dict_request:
+        timeout = dict_request["timeout"]
+    else:
+        timeout = TIMEOUT
+    tmpName = "".join([random.choice(string.ascii_letters + string.digits)
                        for n in range(12)])
-    subprocess.call(["lxc-create", "-n", tmpName, "-t", "debian"])
-    timeout_event = threading.Event()
+    container = lxc.Container(tmpName)
+    container.create("debian", lxc.LXC_CREATE_QUIET)
     try:
-        makeConfigFromTemplate(tmpName)
-        t = threading.Timer(TIMEOUT, timerEvent, (timeout_event, tmpName))
+        #makeConfigFromTemplate(tmpName)
+        container.start()
+        timeout_event = threading.Event()
+        t = threading.Timer(timeout, timerEvent2, (timeout_event, tmpName))
         t.start()
-        rootpath = os.path.join(LXC_PATH, tmpName, "rootfs/")
-        shutil.copy(program_abspath, rootpath)
-        callparams = ["lxc-start", "-n", tmpName,
-                      os.path.join("/", program), "-q"]
-        callparams.extend(params[1:])
-        returncode = subprocess.call(callparams)
+        prog_path = os.path.join(
+            LXC_PATH, tmpName, "rootfs/", os.path.basename(params[0]))
+        fd_prog = io.open(prog_path, "wb")
+        fd_prog.write(program)
+        fd_prog.close()
+        os.chmod(prog_path, stat.S_IEXEC)
+        params[0] = os.path.join("/", os.path.basename(params[0]))
+        returncode = container.attach_wait(
+            lxc.attach_run_command, params, attach_flags=lxc.LXC_ATTACH_DROP_CAPABILITIES, extra_env_vars="PATH=/bin:/sbin:/usr/bin:/usr/sbin")
     except Exception as e:
         returncode = 2
         print(e)
@@ -63,45 +81,62 @@ def processLxc(params):
             t.cancel()
         except:
             pass
-        subprocess.call(["lxc-destroy", "-n", tmpName])
+        if not timeout_event.is_set():
+            if not container.shutdown(5):
+                container.stop()
+        else:
+            container.wait("STOPPED", 10)
+        container.destroy()
     return (returncode, timeout_event.is_set())
 
 
-def composeReplyToClient(client_socket):
-    fd = client_socket.makefile("r")
-    jsonlist = fd.readline()
+def handleRunProgAction(dict_request):
+    program = base64.b64decode(
+        dict_request["b64_data"].encode(encoding="utf8"))
+    (returncode, timeout_occured) = processLxc(dict_request, program)
+    msg = str(returncode % 255)
+    if timeout_occured:
+        msg = "t/o"
+    if returncode == 2:
+        msg = "2 - an unexpected exception occured while trying to run the container"
+    return msg
+
+
+def handleClient(client_socket):
+    fd = client_socket.makefile("rw")
+    keep_alive = True
+    while keep_alive:
+        jsondict = fd.readline()
+        try:
+            dict_request = json.loads(jsondict)
+        except ValueError:
+            fd.write("invalid json object")
+            break
+        else:
+            if "keep-alive" in dict_request:
+                keep_alive = dict_request["keep-alive"]
+            else:
+                keep_alive = False
+            if dict_request["action"] == "run_prog":
+                response = handleRunProgAction(dict_request)
+            fd.write(response + "\n")
+            fd.flush()
     fd.close()
-    try:
-        params = json.loads(jsonlist)
-    except ValueError as e:
-        msg = "invalid json object"
-        print(e)
-    else:
-        (returncode, timeout_occured) = processLxc(params)
-        msg = str(returncode)
-        if timeout_occured:
-            msg = "t/o"
-        if returncode == 2:
-            msg = "2 - an unexpected exception occured while trying to run the container"
-    try:
-        client_socket.sendall(msg.encode())
-    except Exception as e:
-        print(e)
     client_socket.close()
 
 
 def runServer():
     try:
-        os.unlink(SERVER_ADDRESS)
+        os.unlink(UDS_SOCKET)
     except OSError:
         pass
     server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    server_socket.bind(SERVER_ADDRESS)
+    server_socket.bind(UDS_SOCKET)
     server_socket.listen(1)
     while True:
         client_socket, client_address = server_socket.accept()
         client_thread = threading.Thread(
-            target=composeReplyToClient, args=(client_socket, ))
+            target=handleClient, args=(client_socket, ))
         client_thread.start()
 
 
@@ -124,12 +159,10 @@ def main():
     parser = optparse.OptionParser()
     parser.add_option("-b", "--background", action="store_true",
                       dest="background", help="start this process in the background")
-    parser.add_option("-f", "--foreground", action="store_true",
-                      dest="foreground", default=True, help="start this process in the foreground")
     (options, args) = parser.parse_args()
     if options.background:
         sendToBackground()
-    if options.foreground:
+    else:
         runServer()
 
 if __name__ == "__main__":
--- lxc_daemon_client.py
+++ lxc_daemon_client.py
@@ -1,31 +1,33 @@
+import base64
+import io
 import json
-import os
 import socket
 import sys
 
-SERVER_ADDRESS = "./uds_lxcdaemon"
+UDS_SOCKET = "./uds_lxcdaemon"
 
 
 def main():
-    arglist = [os.path.abspath(sys.argv[1])]
-    arglist.extend(sys.argv[2:])
+    dict_request = {"keep-alive": False, "action": "run_prog", "timeout": 30}
+    fd_program = io.open(sys.argv[1], "rb")
+    program = base64.b64encode(fd_program.read())
+    fd_program.close()
+    dict_request["b64_data"] = str(program, encoding="utf8")
+    arglist = sys.argv[1:]
+    dict_request["params"] = arglist
     client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     try:
-    	client_socket.connect(SERVER_ADDRESS)
+        client_socket.connect(UDS_SOCKET)
     except Exception as e:
-    	print(e)
+        print(e)
     else:
-    	try:
-            fd = client_socket.makefile("w")
-            fd.write(json.dumps(arglist))
-            fd.write("\n")
-            fd.close()
-    	except Exception as e:
-    		print(e)
-    	else:
-            reply = client_socket.recv(128).decode()
-            print("lxc_daemon_client: " + reply)
-
+        fd = client_socket.makefile("rw")
+        fd.write(json.dumps(dict_request))
+        fd.write("\n")
+        fd.flush()
+        reply = client_socket.recv(128).decode()
+        fd.close()
+        print("lxc_daemon_client: " + reply.rstrip("\n"))
 
 
 if __name__ == "__main__":
